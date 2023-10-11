3da19fa7cc09b96076486e0f1d6f72772c9be82e
Govindpatidar21 <110691127+Govindpatidar21@users.noreply.github.com>
2023-10-11 11:49:32 +0530

    Update AutoViz_Holo.py

--- autoviz/AutoViz_Holo.py
+++ autoviz/AutoViz_Holo.py
@@ -669,21 +669,20 @@ def draw_distplot_hv(dft, cats, conti, chart_format,problem_type,dep=None,
                 widgets = hv_panel[0]
                 hv_all = pn.Column(pn.Row(*widgets))
             except:
-                print('Error in Distribution Plot')
+                print('Error in Distribution Plot1')
                 hv_panel = []
-        if hv_panel is not []: 
-            if verbose == 2:
-                imgdata_list = append_panels(hv_panel, imgdata_list, chart_format)
-                image_count += 1
-            if chart_format in ['server', 'bokeh_server', 'bokeh-server']:
-                #server = pn.serve(hv_all, start=True, show=True)
-                print('%s can be found in URL below:' %plot_name)
-                hv_all.show()
-            elif chart_format == 'html':
-                save_html_data(hv_all, chart_format, plot_name, mk_dir, additional="_nums")
-            else:
-                display(hv_all)  ### This will display it in a Jupyter Notebook. If you want it on a server, you use drawobj.show()        
-                #display_obj(hv_all)  ### This will display it in a Jupyter Notebook. If you want it on a server, you use drawobj.show()
+        if verbose == 2:
+            imgdata_list = append_panels(hv_panel, imgdata_list, chart_format)
+            image_count += 1
+        if chart_format in ['server', 'bokeh_server', 'bokeh-server']:
+            #server = pn.serve(hv_all, start=True, show=True)
+            print('%s can be found in URL below:' %plot_name)
+            hv_all.show()
+        elif chart_format == 'html':
+            save_html_data(hv_all, chart_format, plot_name, mk_dir, additional="_nums")
+        else:
+            display(hv_all)  ### This will display it in a Jupyter Notebook. If you want it on a server, you use drawobj.show()        
+            #display_obj(hv_all)  ### This will display it in a Jupyter Notebook. If you want it on a server, you use drawobj.show()
     else:
         ######### This is for Classification problems only ########
         transparent = 0.7
