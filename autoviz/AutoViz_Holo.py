ea0699852a1ff021c42acdd5d8f4f7bc518a3394
Govindpatidar21 <110691127+Govindpatidar21@users.noreply.github.com>
2023-10-11 12:22:59 +0530

    Update AutoViz_Holo.py

--- autoviz/AutoViz_Holo.py
+++ autoviz/AutoViz_Holo.py
@@ -669,8 +669,8 @@ def draw_distplot_hv(dft, cats, conti, chart_format,problem_type,dep=None,
                 widgets = hv_panel[0]
                 hv_all = pn.Column(pn.Row(*widgets))
             except:
-                print('Error in Distribution Plot1')
-                hv_panel = None
+                print('Error in Distribution Plot')
+                hv_panel = []
         if verbose == 2:
             imgdata_list = append_panels(hv_panel, imgdata_list, chart_format)
             image_count += 1
@@ -766,7 +766,8 @@ def draw_distplot_hv(dft, cats, conti, chart_format,problem_type,dep=None,
                 widgets = hv_panel[0]
                 hv_all = pn.Column(pn.Row(*widgets))
             except:
-                print('Error in Distribution Plot')
+                print('Error in Distribution Plot1')
+                hv_all=[]
             if verbose == 2:
                 imgdata_list = append_panels(hv_panel, imgdata_list, chart_format)
                 image_count += 1
