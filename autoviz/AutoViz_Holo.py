0767e3b7db4e8812af2adf2a1ebfaf2b12f85e32
govind636 <148571911+govind636@users.noreply.github.com>
2023-11-08 15:30:00 +0530

    Update AutoViz_Holo.py

--- autoviz/AutoViz_Holo.py
+++ autoviz/AutoViz_Holo.py
@@ -17,10 +17,10 @@ import copy
 ########################################
 import logging
 logging.getLogger("param").setLevel(logging.ERROR)
-# from bokeh.util.warnings import BokehUserWarning 
-# import warnings 
-# warnings.simplefilter(action='ignore', category=BokehUserWarning)
-# warnings.filterwarnings("ignore")
+from bokeh.util.warnings import BokehUserWarning 
+import warnings 
+warnings.simplefilter(action='ignore', category=BokehUserWarning)
+warnings.filterwarnings("ignore")
 # from sklearn.exceptions import DataConversionWarning
 # warnings.filterwarnings(action='ignore', category=DataConversionWarning)
 ####################################################################################
@@ -153,7 +153,7 @@ def AutoViz_Holo(filename, sep=',', depVar='', dfte=None, header=0, verbose=0,
     start_time = time.time()
     
     try:
-        print(1)
+       
         dfin, dep,IDcols,bool_vars,cats,nums,discrete_string_vars,date_vars,classes,problem_type,selected_cols = classify_print_vars(
                                             filename,sep,max_rows_analyzed, max_cols_analyzed,
                                             depVar,dfte,header,verbose)
