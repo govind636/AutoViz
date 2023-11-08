86a3d54df2da376ed3d2d4b6c859ef0c89212d4a
govind636 <148571911+govind636@users.noreply.github.com>
2023-11-08 13:41:38 +0530

    Update __init__.py

--- autoviz/__init__.py
+++ autoviz/__init__.py
@@ -2,7 +2,7 @@ name = "autoviz"
 from .__version__ import __version__, __holo_version__
 from .AutoViz_Class import AutoViz_Class
 # from .AutoViz_Class import data_cleaning_suggestions
-from .AutoViz_Class import FixDQ
+# from .AutoViz_Class import FixDQ
 if __name__ == "__main__":
     module_type = 'Running'
 else:
